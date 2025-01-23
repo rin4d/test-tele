6c4120429964bd9d54c14ca7656e1f94a4f20a3b
RIN4D <167313776+rin4d@users.noreply.github.com>
2025-01-23 18:10:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function sendCommand(command) {
     const url = `http://172.24.1.2:8080/cgi-bin/do.sh?key=${command}`;
+    console.log(`Envoi de la commande : ${url}`);
 
     fetch(url, {
-        method: 'GET', // Envoie une requête GET
-        mode: 'no-cors' // Désactive la vérification CORS
+        method: 'GET'
     })
-    .then(() => {
-        console.log(`Commande envoyée : ${url}`);
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP : ${response.status}`);
+        }
+        console.log(`Commande envoyée avec succès : ${url}`);
     })
     .catch(error => {
         console.error('Erreur lors de l’envoi de la commande :', error);
