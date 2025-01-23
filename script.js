df9167f6c4a156127f46455832e98c4adc88aaf2
RIN4D <167313776+rin4d@users.noreply.github.com>
2025-01-23 17:50:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function sendCommand(command) {
+    const url = `http://172.24.1.1:8080/cgi-bin/do.sh?key=${command}`;
+
+    fetch(url, {
+        method: 'GET', // Envoie une requête GET
+        mode: 'no-cors' // Désactive la vérification CORS
+    })
+    .then(() => {
+        console.log(`Commande envoyée : ${url}`);
+    })
+    .catch(error => {
+        console.error('Erreur lors de l’envoi de la commande :', error);
+        alert('Impossible de se connecter au serveur.');
+    });
+}
